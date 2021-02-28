0341777555ef8e47c764c1959ccbc2d6f55197aa
Banee Ishaque K <baneeishaque@hotmail.com>
2021-02-28 13:43:27 +0000

    Typo

--- src/main/java/PNB_Transaction_Importer_Gradle/Main.java
+++ src/main/java/PNB_Transaction_Importer_Gradle/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PNB_Transaction_Importer_Gradle;
 
 import com.opencsv.CSVReader;
@@ -12,7 +7,7 @@ import java.io.IOException;
 
 /**
  *
- * @author Admin
+ * @author Banee Ishaque K
  */
 public class Main {
 
